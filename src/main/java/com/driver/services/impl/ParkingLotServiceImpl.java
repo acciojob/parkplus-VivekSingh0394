f97510d3930f8409b2e6ecc7ddcfbc1190b5ffb9
Ravi <rksnhes1999@gmail.com>
2023-01-28 19:05:59 +0530

    ge

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -32,42 +32,41 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     @Override
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
         // get parking lot of given id
-          ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
-          // create spot
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        // create spot
         Spot spot = new Spot();
         //setting spot atrribbutes perhr,occuied,parkinglot,spottype
         spot.setPricePerHour(pricePerHour);
         spot.setOccupied(false);
         spot.setParkingLot(parkingLot);
         SpotType spotType;
-        if(numberOfWheels ==2)
-        {
-            spotType=SpotType.TWO_WHEELER;
+        if (numberOfWheels == 2) {
+            spotType = SpotType.TWO_WHEELER;
+            spot.setSpotType(SpotType.TWO_WHEELER);
         }
-        else if(numberOfWheels == 4)
-        {
-            spotType=SpotType.FOUR_WHEELER;
+        if (numberOfWheels == 4) {
+            spotType = SpotType.FOUR_WHEELER;
+            spot.setSpotType(SpotType.FOUR_WHEELER);
         }
-        else
-        {
-            spotType =SpotType.OTHERS;
-        }
-        spot.setSpotType(spotType);
-        List<Spot> spotList = parkingLot.getSpotList();
-        if(spotList == null)
-        {
-            spotList = new ArrayList<>();
-        }
-        spotList.add(spot);
-        // set spot in parkinh lot
-        parkingLot.setSpotList(spotList);
-        // save parking lot
-        parkingLotRepository1.save(parkingLot);
-        spotRepository1.save(spot);
-        return spot;
+            if (numberOfWheels > 4) {
+                spotType = SpotType.OTHERS;
+                spot.setSpotType(SpotType.OTHERS);
+            }
+
+            List<Spot> spotList = parkingLot.getSpotList();
+            if (spotList == null) {
+                spotList = new ArrayList<>();
+            }
+            spotList.add(spot);
+            // set spot in parkinh lot
+            parkingLot.setSpotList(spotList);
+            // save parking lot
+            parkingLotRepository1.save(parkingLot);
+            spotRepository1.save(spot);
+            return spot;
+
 
     }
-
     @Override
     public void deleteSpot(int spotId) {
         Spot spot = spotRepository1.findById(spotId).get();
